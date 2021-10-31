0326b819021aaa809a74cba4cc9b1a97a5a14f93
Ojal M <ojalmaps@gmail.com>
2021-10-30 23:05:48 -0700

    Adding more methods

--- simpleCSV/file_analyze.py
+++ simpleCSV/file_analyze.py
@@ -1,20 +1,25 @@
 import csv
-iq = 0
-height = 0
-brain = 0
-weight = 0
-n = 0
-with open("assignment.csv") as assignment:
-    file = csv.reader(assignment)
-    next(file)
-    for row in file:
-        iq += float(row[0])
-        brain += float(row[1])
-        height += float(row[2])
-        weight += float(row[3])
-        n += 1
 
-print( " Average height "+ str(height / n))
-print(" Average weight "+ str(weight / n))
-print(" Average brain size "+ str(brain / n))
-print(" Average iq "+ str(iq / n))
+def average_data():
+    iq = 0
+    height = 0
+    brain = 0
+    weight = 0
+    n = 0
+
+    with open("braindata.csv") as brain_data:
+        file = csv.reader(brain_data)
+        next(file) # skipping header row
+        for row in file:
+            iq += float(row[0])
+            brain += float(row[1])
+            height += float(row[2])
+            weight += float(row[3])
+            n += 1
+    print( " Some facts about the brain" )
+    print( " Average height "+ str(height / n))
+    print(" Average weight "+ str(weight / n))
+    print(" Average brain size "+ str(brain / n))
+    print(" Average iq "+ str(iq / n))
+
+average_data()
